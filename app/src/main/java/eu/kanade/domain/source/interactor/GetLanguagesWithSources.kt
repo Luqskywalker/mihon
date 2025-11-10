9904086734c03dcde94fcc7d67d4239b9da10ff3
Luqskywalker <87660844+Luqskywalker@users.noreply.github.com>
2025-11-10 22:30:29 +0800

    Update GetLanguagesWithSources.kt

--- app/src/main/java/eu/kanade/domain/source/interactor/GetLanguagesWithSources.kt
+++ app/src/main/java/eu/kanade/domain/source/interactor/GetLanguagesWithSources.kt
@@ -4,6 +4,7 @@ import eu.kanade.domain.source.service.SourcePreferences
 import eu.kanade.tachiyomi.util.system.LocaleHelper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import tachiyomi.domain.source.model.Source
 import tachiyomi.domain.source.repository.SourceRepository
 import java.util.SortedMap
@@ -13,22 +14,22 @@ class GetLanguagesWithSources(
     private val preferences: SourcePreferences,
 ) {
 
-    fun subscribe(): Flow<SortedMap<String, List<Source>>> {
-        return combine(
-            preferences.enabledLanguages().changes(),
-            preferences.disabledSources().changes(),
-            repository.getOnlineSources(),
-        ) { enabledLanguage, disabledSource, onlineSources ->
-            val sortedSources = onlineSources.sortedWith(
-                compareBy<Source> { it.id.toString() in disabledSource }
-                    .thenBy(String.CASE_INSENSITIVE_ORDER) { it.name },
-            )
-
-            sortedSources
-                .groupBy { it.lang }
-                .toSortedMap(
-                    compareBy<String> { it !in enabledLanguage }.then(LocaleHelper.comparator),
-                )
-        }
+    fun subscribe(): Flow<SortedMap<String, List<Source>>> = combine(
+        preferences.enabledLanguages().changes(),
+        preferences.disabledSources().changes(),
+        repository.getOnlineSources(),
+    ) { enabledLangs, disabledIds, sources ->
+        sources
+            .sortedWith(compareSourcePriority(disabledIds))
+            .groupBy { it.lang }
+            .toSortedMap(compareLanguagePriority(enabledLangs))
     }
+
+    private fun compareSourcePriority(disabledIds: Set<String>) = 
+        compareBy<Source> { it.id.toString() in disabledIds }
+            .thenBy { it.name.lowercase() }
+
+    private fun compareLanguagePriority(enabledLangs: Set<String>) = 
+        compareBy<String> { it !in enabledLangs }
+            .then(LocaleHelper.comparator)
 }
