cd1cf38987e09d38a009dedd19439547498a9996
Luqskywalker <87660844+Luqskywalker@users.noreply.github.com>
2025-11-10 22:34:03 +0800

    Update ToggleSource.kt

--- app/src/main/java/eu/kanade/domain/source/interactor/ToggleSource.kt
+++ app/src/main/java/eu/kanade/domain/source/interactor/ToggleSource.kt
@@ -8,24 +8,31 @@ class ToggleSource(
     private val preferences: SourcePreferences,
 ) {
 
-    fun await(source: Source, enable: Boolean = isEnabled(source.id)) {
-        await(source.id, enable)
-    }
+    fun enable(source: Source) = set(source.id, enabled = true)
+    fun enable(sourceId: Long) = set(sourceId, enabled = true)
+    fun enable(sourceIds: List<Long>) = set(sourceIds, enabled = true)
 
-    fun await(sourceId: Long, enable: Boolean = isEnabled(sourceId)) {
+    fun disable(source: Source) = set(source.id, enabled = false)
+    fun disable(sourceId: Long) = set(sourceId, enabled = false)
+    fun disable(sourceIds: List<Long>) = set(sourceIds, enabled = false)
+
+    fun toggle(source: Source) = set(source.id, enabled = !isEnabled(source.id))
+    fun toggle(sourceId: Long) = set(sourceId, enabled = !isEnabled(sourceId))
+
+    fun set(source: Source, enabled: Boolean) = set(source.id, enabled)
+    fun set(sourceId: Long, enabled: Boolean) {
         preferences.disabledSources().getAndSet { disabled ->
-            if (enable) disabled.minus("$sourceId") else disabled.plus("$sourceId")
+            if (enabled) disabled - "$sourceId" else disabled + "$sourceId"
         }
     }
 
-    fun await(sourceIds: List<Long>, enable: Boolean) {
-        val transformedSourceIds = sourceIds.map { it.toString() }
+    fun set(sourceIds: List<Long>, enabled: Boolean) {
+        val ids = sourceIds.map { it.toString() }
         preferences.disabledSources().getAndSet { disabled ->
-            if (enable) disabled.minus(transformedSourceIds) else disabled.plus(transformedSourceIds)
+            if (enabled) disabled - ids else disabled + ids
         }
     }
 
-    private fun isEnabled(sourceId: Long): Boolean {
-        return sourceId.toString() in preferences.disabledSources().get()
-    }
+    fun isEnabled(source: Source): Boolean = isEnabled(source.id)
+    fun isEnabled(sourceId: Long): Boolean = "$sourceId" !in preferences.disabledSources().get()
 }
