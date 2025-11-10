5918f6c9b9458881b386685190adf19760092128
Luqskywalker <87660844+Luqskywalker@users.noreply.github.com>
2025-11-10 22:16:55 +0800

    Update GetExtensionSources.kt

--- app/src/main/java/eu/kanade/domain/extension/interactor/GetExtensionSources.kt
+++ app/src/main/java/eu/kanade/domain/extension/interactor/GetExtensionSources.kt
@@ -10,24 +10,19 @@ class GetExtensionSources(
     private val preferences: SourcePreferences,
 ) {
 
-    fun subscribe(extension: Extension.Installed): Flow<List<ExtensionSourceItem>> {
-        val isMultiSource = extension.sources.size > 1
-        val isMultiLangSingleSource =
-            isMultiSource && extension.sources.map { it.name }.distinct().size == 1
-
-        return preferences.disabledSources().changes().map { disabledSources ->
-            fun Source.isEnabled() = id.toString() !in disabledSources
-
-            extension.sources
-                .map { source ->
-                    ExtensionSourceItem(
-                        source = source,
-                        enabled = source.isEnabled(),
-                        labelAsName = isMultiSource && !isMultiLangSingleSource,
-                    )
-                }
+    fun subscribe(extension: Extension.Installed): Flow<List<ExtensionSourceItem>> =
+        preferences.disabledSources().changes().map { disabledSources ->
+            val isMultiSource = extension.sources.size > 1
+            val isMultiLangSingleSource = isMultiSource && extension.sources.map { it.name }.distinct().size == 1
+            
+            extension.sources.map { source ->
+                ExtensionSourceItem(
+                    source = source,
+                    enabled = source.id.toString() !in disabledSources,
+                    labelAsName = isMultiSource && !isMultiLangSingleSource,
+                )
+            }
         }
-    }
 }
 
 data class ExtensionSourceItem(
