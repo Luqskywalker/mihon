ad6e91f6049ad37dffef57bf6f83467f9275ce42
Luqskywalker <87660844+Luqskywalker@users.noreply.github.com>
2025-11-10 22:22:28 +0800

    Update SetExcludedScanlators.kt

--- app/src/main/java/eu/kanade/domain/manga/interactor/SetExcludedScanlators.kt
+++ app/src/main/java/eu/kanade/domain/manga/interactor/SetExcludedScanlators.kt
@@ -8,14 +8,17 @@ class SetExcludedScanlators(
 
     suspend fun await(mangaId: Long, excludedScanlators: Set<String>) {
         handler.await(inTransaction = true) {
-            val currentExcluded = handler.awaitList {
-                excluded_scanlatorsQueries.getExcludedScanlatorsByMangaId(mangaId)
-            }.toSet()
-            val toAdd = excludedScanlators.minus(currentExcluded)
-            for (scanlator in toAdd) {
+            val currentExcluded = excluded_scanlatorsQueries
+                .getExcludedScanlatorsByMangaId(mangaId)
+                .executeAsList()
+                .toSet()
+
+            val toAdd = excludedScanlators - currentExcluded
+            toAdd.forEach { scanlator ->
                 excluded_scanlatorsQueries.insert(mangaId, scanlator)
             }
-            val toRemove = currentExcluded.minus(excludedScanlators)
+
+            val toRemove = currentExcluded - excludedScanlators
             excluded_scanlatorsQueries.remove(mangaId, toRemove)
         }
     }
