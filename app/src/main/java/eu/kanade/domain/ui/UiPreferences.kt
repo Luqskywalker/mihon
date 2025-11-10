6edb928d1e17add49632ea711ca90b92345bc543
Luqskywalker <87660844+Luqskywalker@users.noreply.github.com>
2025-11-10 22:48:36 +0800

    Update UiPreferences.kt

--- app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
+++ app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
@@ -15,31 +15,36 @@ class UiPreferences(
     private val preferenceStore: PreferenceStore,
 ) {
 
-    fun themeMode() = preferenceStore.getEnum("pref_theme_mode_key", ThemeMode.SYSTEM)
+    fun themeMode() = preferenceStore.getEnum("theme_mode", ThemeMode.SYSTEM)
 
     fun appTheme() = preferenceStore.getEnum(
-        "pref_app_theme",
-        if (DeviceUtil.isDynamicColorAvailable) {
-            AppTheme.MONET
-        } else {
-            AppTheme.DEFAULT
-        },
+        "app_theme",
+        if (DeviceUtil.isDynamicColorAvailable) AppTheme.MONET else AppTheme.DEFAULT,
     )
 
-    fun themeDarkAmoled() = preferenceStore.getBoolean("pref_theme_dark_amoled_key", false)
+    fun themeDarkAmoled() = preferenceStore.getBoolean("theme_dark_amoled", false)
 
-    fun relativeTime() = preferenceStore.getBoolean("relative_time_v2", true)
+    fun relativeTime() = preferenceStore.getBoolean("relative_time", true)
 
-    fun dateFormat() = preferenceStore.getString("app_date_format", "")
+    fun dateFormat() = preferenceStore.getString("date_format", "")
 
     fun tabletUiMode() = preferenceStore.getEnum("tablet_ui_mode", TabletUiMode.AUTOMATIC)
 
-    fun imagesInDescription() = preferenceStore.getBoolean("pref_render_images_description", true)
+    fun imagesInDescription() = preferenceStore.getBoolean("images_in_description", true)
+
+    fun hideBottomNavigationOnScroll() = preferenceStore.getBoolean("hide_bottom_nav_on_scroll", true)
+
+    fun useGridForLibrary() = preferenceStore.getBoolean("use_grid_library", false)
+
+    fun showLibraryTabs() = preferenceStore.getBoolean("show_library_tabs", true)
 
     companion object {
         fun dateFormat(format: String): DateTimeFormatter = when (format) {
             "" -> DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
             else -> DateTimeFormatter.ofPattern(format, Locale.getDefault())
         }
+
+        val defaultAppTheme: AppTheme
+            get() = if (DeviceUtil.isDynamicColorAvailable) AppTheme.MONET else AppTheme.DEFAULT
     }
 }
