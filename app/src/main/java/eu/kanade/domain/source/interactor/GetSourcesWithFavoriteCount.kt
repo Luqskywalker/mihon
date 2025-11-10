57dee207514b89f0ef0962975433bc3f575047fc
Luqskywalker <87660844+Luqskywalker@users.noreply.github.com>
2025-11-10 22:31:29 +0800

    Update GetSourcesWithFavoriteCount.kt

--- app/src/main/java/eu/kanade/domain/source/interactor/GetSourcesWithFavoriteCount.kt
+++ app/src/main/java/eu/kanade/domain/source/interactor/GetSourcesWithFavoriteCount.kt
@@ -3,55 +3,53 @@ package eu.kanade.domain.source.interactor
 import eu.kanade.domain.source.service.SourcePreferences
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import tachiyomi.core.common.util.lang.compareToWithCollator
 import tachiyomi.domain.source.model.Source
 import tachiyomi.domain.source.repository.SourceRepository
 import tachiyomi.source.local.isLocal
-import java.util.Collections
 
 class GetSourcesWithFavoriteCount(
     private val repository: SourceRepository,
     private val preferences: SourcePreferences,
 ) {
 
-    fun subscribe(): Flow<List<Pair<Source, Long>>> {
-        return combine(
-            preferences.migrationSortingDirection().changes(),
-            preferences.migrationSortingMode().changes(),
-            repository.getSourcesWithFavoriteCount(),
-        ) { direction, mode, list ->
-            list
-                .filterNot { it.first.isLocal() }
-                .sortedWith(sortFn(direction, mode))
-        }
+    fun subscribe(): Flow<List<Pair<Source, Long>>> = combine(
+        preferences.migrationSortingDirection().changes(),
+        preferences.migrationSortingMode().changes(),
+        repository.getSourcesWithFavoriteCount(),
+    ) { direction, mode, sources ->
+        sources
+            .filterNot { (source, _) -> source.isLocal() }
+            .sortedWith(createComparator(direction, mode))
     }
 
-    private fun sortFn(
+    private fun createComparator(
         direction: SetMigrateSorting.Direction,
-        sorting: SetMigrateSorting.Mode,
-    ): java.util.Comparator<Pair<Source, Long>> {
-        val sortFn: (Pair<Source, Long>, Pair<Source, Long>) -> Int = { a, b ->
-            when (sorting) {
-                SetMigrateSorting.Mode.ALPHABETICAL -> {
-                    when {
-                        a.first.isStub && !b.first.isStub -> -1
-                        b.first.isStub && !a.first.isStub -> 1
-                        else -> a.first.name.lowercase().compareToWithCollator(b.first.name.lowercase())
-                    }
+        mode: SetMigrateSorting.Mode,
+    ): Comparator<Pair<Source, Long>> {
+        val baseComparator = when (mode) {
+            SetMigrateSorting.Mode.ALPHABETICAL -> compareBy<Pair<Source, Long>> { (source, _) ->
+                when {
+                    source.isStub -> ""
+                    else -> source.name.lowercase()
                 }
-                SetMigrateSorting.Mode.TOTAL -> {
-                    when {
-                        a.first.isStub && !b.first.isStub -> -1
-                        b.first.isStub && !a.first.isStub -> 1
-                        else -> a.second.compareTo(b.second)
-                    }
+            }.thenComparator { a, b ->
+                when {
+                    a.first.isStub && !b.first.isStub -> -1
+                    b.first.isStub && !a.first.isStub -> 1
+                    else -> a.first.name.compareToWithCollator(b.first.name)
                 }
             }
+            
+            SetMigrateSorting.Mode.TOTAL -> compareBy<Pair<Source, Long>> { (source, _) ->
+                !source.isStub
+            }.thenBy { (_, count) -> count }
         }
 
         return when (direction) {
-            SetMigrateSorting.Direction.ASCENDING -> Comparator(sortFn)
-            SetMigrateSorting.Direction.DESCENDING -> Collections.reverseOrder(sortFn)
+            SetMigrateSorting.Direction.ASCENDING -> baseComparator
+            SetMigrateSorting.Direction.DESCENDING -> baseComparator.reversed()
         }
     }
 }
