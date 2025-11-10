17ebe29c6ea7fdd24624d1316dba8e4b9fea5fca
Luqskywalker <87660844+Luqskywalker@users.noreply.github.com>
2025-11-10 22:16:06 +0800

    Update GetExtensionLanguages.kt

--- app/src/main/java/eu/kanade/domain/extension/interactor/GetExtensionLanguages.kt
+++ app/src/main/java/eu/kanade/domain/extension/interactor/GetExtensionLanguages.kt
@@ -5,28 +5,32 @@ import eu.kanade.tachiyomi.extension.ExtensionManager
 import eu.kanade.tachiyomi.util.system.LocaleHelper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 
 class GetExtensionLanguages(
     private val preferences: SourcePreferences,
     private val extensionManager: ExtensionManager,
 ) {
-    fun subscribe(): Flow<List<String>> {
-        return combine(
-            preferences.enabledLanguages().changes(),
-            extensionManager.availableExtensionsFlow,
-        ) { enabledLanguage, availableExtensions ->
-            availableExtensions
-                .flatMap { ext ->
-                    if (ext.sources.isEmpty()) {
-                        listOf(ext.lang)
-                    } else {
-                        ext.sources.map { it.lang }
-                    }
-                }
-                .distinct()
-                .sortedWith(
-                    compareBy<String> { it !in enabledLanguage }.then(LocaleHelper.comparator),
-                )
-        }
+
+    fun subscribe(): Flow<List<String>> = combine(
+        preferences.enabledLanguages().changes(),
+        extensionManager.availableExtensionsFlow,
+    ) { enabledLanguages, extensions ->
+        extensions
+            .getAllLanguages()
+            .distinct()
+            .sortedWithLanguagesFirst(enabledLanguages)
     }
+
+    private fun List<ExtensionManager.ExtensionItem>.getAllLanguages(): List<String> = 
+        flatMap { ext -> 
+            if (ext.sources.isEmpty()) listOf(ext.lang) 
+            else ext.sources.map { it.lang } 
+        }
+
+    private fun List<String>.sortedWithLanguagesFirst(enabledLanguages: Set<String>) = 
+        sortedWith(
+            compareBy<String> { it !in enabledLanguages }
+                .then(LocaleHelper.comparator)
+        )
 }
